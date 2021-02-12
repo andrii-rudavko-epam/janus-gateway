4a3741b02614e82902e4ea30a76afcbd3c43e0d6
Andrii Rudavko <78725629+andrii-rudavko-epam@users.noreply.github.com>
2021-02-12 05:54:50 +0200

    Update rollup.config.js

--- npm/rollup.config.js
+++ npm/rollup.config.js
@@ -2,16 +2,16 @@ import replace from 'rollup-plugin-replace';
 import * as fs from 'fs';
 
 export default {
-    name: 'Janus',
-    input: 'module.js',
-    output: {
-        strict: false
-    },
-    plugins: [
-        replace({
-            JANUS_CODE: fs.readFileSync('../html/janus.js', 'utf-8'),
-            delimiters: ['@','@'],
-            includes: 'module.js'
-        })
-    ]
+  name: 'Janus',
+  input: './npm/module.js',
+  output: {
+    strict: false
+  },
+  plugins: [
+    replace({
+      JANUS_CODE: fs.readFileSync('./html/janus.js', 'utf-8'),
+      delimiters: ['@', '@'],
+      includes: 'module.js'
+    })
+  ]
 };
